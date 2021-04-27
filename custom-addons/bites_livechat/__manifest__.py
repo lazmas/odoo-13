86cfed7ff97bf222547af6be267c75df60eb6a4b
lazmas <audrius@sik.lt>
2021-04-27 10:57:16 +0300

    [ADD]

---
+++ custom-addons/bites_livechat/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Audrius Lekavičius - Dextera
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Bitės livechat modulis",
+    "summary": "Bitės teikiamos paslaugos livechat modulis",
+    "author": "Audrius Lekavičius - Dextera",
+    "email": "audrius@sik.lt",
+    "website": "https://dextera.lt",
+    "category": "Discuss",
+    "version": "13.0.1.0.0",
+    "depends": ["base_setup"],
+    "data": [
+        "views/bites_chat_assets.xml"
+    ],
+}
